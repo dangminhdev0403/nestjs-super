fd86ffa179c791e754fe64f55b45682bc973e09b
Your Name <you@example.com>
2025-11-17 18:22:57 +0700

    add validation register

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,30 @@
-import { Logger } from '@nestjs/common';
+import { Logger, UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Server } from 'http';
 import { AddressInfo } from 'net';
-import { envConfig } from 'src/shared/config';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+      transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+      transformOptions: { enableImplicitConversion: true }, // Enable implicit type conversion
+      exceptionFactory(errors) {
+        return new UnprocessableEntityException(
+          errors.map((err) => ({
+            property: err.property,
+            constraints: Object.values(err.constraints ?? {}).join(', '),
+          })),
+        );
+      },
+    }),
+  );
   const port = parseInt(process.env.PORT ?? '3000');
   await app.listen(port);
   const server = app.getHttpServer() as Server;
   const address = server.address() as AddressInfo | string;
-  envConfig.ACCESS_TOKEN_EXPIRES_IN;
   const host = typeof address === 'string' ? address : address.address;
   const actualHost = host === '::' || host === '0.0.0.0' ? 'localhost' : host;
   Logger.log(`🚀 Application is running on: http://${actualHost}:${port}`, 'Bootstrap');
--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -3,13 +3,14 @@ https://docs.nestjs.com/controllers#controllers
 */
 
 import { Body, Controller, Post } from '@nestjs/common';
+import { RegisterBodyDTO } from 'src/routes/auth/auth.dto';
 import { AuthService } from 'src/routes/auth/auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('register')
-  registerUser(@Body() body: any) {
+  registerUser(@Body() body: RegisterBodyDTO) {
     return this.authService.registerUser(body);
   }
 }
