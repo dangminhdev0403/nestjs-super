44f720977f6102e356f62daad33d06235fdcae27
Your Name <you@example.com>
2025-11-17 18:57:27 +0700

    update register with dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './routes/auth/auth.controller';
@@ -7,9 +8,17 @@ import { PostsModule } from './routes/posts/posts.module';
 import { HashingService } from './shared/services/hashing.service';
 import { SharedModule } from './shared/shared.module';
 
+const service = [HashingService, AppService];
+
 @Module({
   imports: [AuthModule, SharedModule, PostsModule, AuthModule],
   controllers: [AuthController, AppController],
-  providers: [HashingService, AppService],
+  providers: [
+    ...service,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -2,15 +2,17 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Post } from '@nestjs/common';
-import { RegisterBodyDTO } from 'src/routes/auth/auth.dto';
+import { Body, Controller, Post, SerializeOptions } from '@nestjs/common';
+import { RegisterBodyDTO, RegisterResDTO } from 'src/routes/auth/auth.dto';
 import { AuthService } from 'src/routes/auth/auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @SerializeOptions({ type: RegisterResDTO })
   @Post('register')
-  registerUser(@Body() body: RegisterBodyDTO) {
-    return this.authService.registerUser(body);
+  async registerUser(@Body() body: RegisterBodyDTO) {
+    const result = await this.authService.registerUser(body);
+    return result;
   }
 }
